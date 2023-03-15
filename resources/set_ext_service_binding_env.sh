12793a0ed4c14c7a39f495c9767cdeed82c45e3e
Tola Awofolu <oawofolu@pivotal.io>
2023-03-15 08:08:38 -0400

    Updated script

--- resources/set_ext_service_binding_env.sh
+++ resources/set_ext_service_binding_env.sh
@@ -1,8 +1,11 @@
-TYPE=MYTYPE
-PROVIDER=MYPROVIDER
-HOST=MYHOST
-PORT=MYPORT
-DATABASE=MYDATABASE
-USERNAME=MYUSERNAME
-PASSWORD=MYPASSWORD
+export TYPE=MYTYPE
+export PROVIDER=MYPROVIDER
+export HOST=MYHOST
+export PORT=MYPORT
+export DATABASE=MYDATABASE
+export USERNAME=MYUSERNAME
+export PASSWORD=MYPASSWORD
 envsubst < resources/service-binding-secret.template.yaml > resources/service-binding-secret.yaml
+kubectl apply -f resources/stk-secret-reader.yaml -n MYNAMESPACE
+ytt -f resources/service-binding-secret.yaml -f service-claim.yaml -n MYNAMESPACE
+kubectl get resourceclaim MYCLAIM -n MYNAMESPACE
