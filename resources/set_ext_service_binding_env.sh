b0ba9aad901abaad7934740f2ab0467dfd58f35c
Tola Awofolu <oawofolu@pivotal.io>
2023-03-15 07:39:46 -0400

    Added resources

--- resources/set_ext_service_binding_env.sh
+++ resources/set_ext_service_binding_env.sh
@@ -1,8 +1,8 @@
-TYPE=_type_
-PROVIDER=_provider_
-HOST=_host_
-PORT=_port_
-DATABASE=_database_
-USERNAME=_username_
-PASSWORD=_password_
+TYPE=MYTYPE
+PROVIDER=MYPROVIDER
+HOST=MYHOST
+PORT=MYPORT
+DATABASE=MYDATABASE
+USERNAME=MYUSERNAME
+PASSWORD=MYPASSWORD
 envsubst < resources/service-binding-secret.template.taml > resources/service-binding-secret.yaml
