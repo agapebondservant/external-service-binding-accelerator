9f1080e34f0c6de5470f29cef9609d5cf4002bd2
Tola Awofolu <oawofolu@pivotal.io>
2023-12-14 07:50:22 -0500

    Added fields

--- resources/set_ext_service_binding_env.sh
+++ resources/set_ext_service_binding_env.sh
@@ -9,4 +9,6 @@ envsubst < resources/service-binding-secret.template.yaml > resources/service-bi
 kubectl apply -f resources/stk-secret-reader.yaml -n MYNAMESPACE
 kubectl apply -f resources/service-binding-secret.yaml -n MYNAMESPACE
 kubectl apply -f resources/service-claim.yaml -n MYNAMESPACE
+kubectl apply -f resources/class-claim.yaml -n BACKSTAGE_TARGET_NAMESPACE
 kubectl get resourceclaim MYCLAIM -n MYNAMESPACE
+kubectl get classclaim -n MYNAMESPACE
